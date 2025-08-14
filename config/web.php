c3494bb5f7abff081493e5078fe4eabbea661448
ttyk <rojroe@gmail.com>
2025-08-14 14:43:12 +0500

    Первый коммит: установка Yii2

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'asf98h1h23h8fhsd83hf983hfs9dfh',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
